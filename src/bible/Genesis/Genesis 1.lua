12e373c9fa32d464ba13daf696e21984c8afc99d
Uptight <RobloxArchiver@protonmail.com>
2022-03-25 23:51:11 -0400

    Added Genesis 1
    If theirs any issues or parts missing please feel free to DM me my mistake!

---
+++ src/bible/Genesis/Genesis 1.lua
@@ -0,0 +1,81 @@
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/RobloxArchiver/PreacherBot/main/src/backend/mainapi.lua"))()
+
+local msg = pb.SayPreach
+
+-- Times calculated by: https://edgestudio.com/words-to-time-calculator/
+-- Bible used: https://vulgate.org/ot/
+
+-- Speed: 3 WPS
+
+msg("In the beginning God created heaven, and earth.", 2) 
+-- Genesis 1:1
+msg("And the earth was void and empty, and darkness was upon the face of the deep; and the spirit of God moved over the waters.", 8) 
+-- Genesis 1:2
+msg("And God said: Be light made. And light was made.", 3) 
+-- Genesis 1:3
+msg("And God saw the light that it was good; and he divided the light from the darkness.", 5) 
+-- Genesis 1:4
+msg("And he called the light Day, and the darkness Night; and there was evening and morning one day.", 6) 
+-- Genesis 1:5
+msg("And God said: Let there be a firmament made amidst the waters: and let it divide the waters from the waters.", 7) 
+-- Genesis 1:6
+msg("And God made a firmament, and divided the waters that were under the firmament, from those that were above the firmament, and it was so.", 8)
+-- Genesis 1:7
+msg("And God called the firmament, Heaven; and the evening and morning were the second day.", 5) 
+-- Genesis 1:8
+msg("God also said: Let the waters that are under the heaven, be gathered together into one place: and let the dry land appear. And it was so done.", 9) 
+-- Genesis 1:9
+msg("And God called the dry land, Earth; and the gathering together of the waters, he called Seas. And God saw that it was good.", 8) 
+-- Genesis 1:10
+msg("And he said: Let the earth bring forth the green herb, and such as may seed, and the fruit tree yielding fruit after its kind", 8) 
+-- Genesis 1:11
+msg("which may have seed in itself upon the earth. And it was so done.", 4) 
+-- Genesis 1:11 Continued
+msg("And the earth brought forth the green herb, and such as yieldeth seed according to its kind, and the tree that beareth fruit having seed each one according to its kind.", 10)
+-- Genesis 1:12
+msg("And God saw that it was good.", 2)
+-- Genesis 1:12 Continued
+msg("And the evening and the morning were the third day.", 3)
+-- Genesis 1:13
+msg("And God said: Let there be lights made in the firmament of heaven, to divide the day and the night, and let them be for signs, and for seasons", 9)
+-- Genesis 1:14
+msg("and for days and years:", 1)
+-- Genesis 1:14 Continued
+msg("To shine in the firmament of heaven, and to give light upon the earth. And it was so done.", 6)
+-- Genesis 1:15
+msg("And God made two great lights: a greater light to rule the day; and a lesser light to rule the night: and the stars.", 8)
+-- Genesis 1:16
+msg("And he set them in the firmament of heaven to shine upon the earth.", 4)
+-- Genesis 1:17
+msg("And to rule the day and the night, and to divide the light and the darkness. And God saw that it was good.", 7)
+-- Genesis 1:18
+msg("And the evening and morning were the fourth day.", 3)
+-- Genesis 1:19
+msg("God also said: Let the waters bring forth the creeping creature having life, and the fowl that may fly over the earth under the firmament of heaven.", 9)
+-- Genesis 1:20
+msg("And God created the great whales, and every living and moving creature, which the waters brought forth, according to their kinds,", 7)
+-- Genesis 1:21
+msg("and every winged fowl according to its kind. And God saw that it was good.", 5)
+-- Genesis 1:21 Continued
+msg("And he blessed them, saying: Increase and multiply, and fill the waters of the sea: and let the birds be multiplied upon the earth.", 8)
+-- Genesis 1:22
+msg("And the evening and morning were the fifth day.", 3)
+-- Genesis 1:23
+msg("And God said: Let the earth bring forth the living creature in its kind, cattle and creeping things, and beasts of the earth, according to their kinds. And it was so done.", 10)
+-- Genesis 1:24
+msg("And God made the beasts of the earth according to their kinds, and cattle, and every thing that creepeth on the earth after its kind. And God saw that it was good.", 10)
+-- Genesis 1:25
+msg("And he said: Let us make man to our image and likeness: and let him have dominion over the fishes of the sea", 7)
+-- Genesis 1:26
+msg("and the fowls of the air, and the beasts, and the whole earth, and every creeping creature that moveth upon the earth.", 7)
+-- Genesis 1:26 Continued
+msg("And God created man to his own image: to the image of God he created him: male and female he created them.", 7)
+-- Genesis 1:27
+msg("and all living creatures that move upon the earth.", 3)
+-- Genesis 1:27 Continued
+msg("And God said: Behold I have given you every herb bearing seed upon the earth, and all trees that have in themselves seed of their own kind, to be your meat:", 10)
+-- Genesis 1:28
+msg("And to all the beasts of the earth, and to every fowl of the air, and to all that move upon the earth, and wherein there is life, that they may have to feed upon. And it was so done.", 13)
+-- Genesis 1:29
+msg("And God saw all the things that he had made, and they were very good. And the evening and morning were the sixth day.", 8)
+-- Genesis 1:30
