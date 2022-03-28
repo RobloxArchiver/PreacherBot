c5d4fee86eb20993644f022bd8ad67278fea4291
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-03-28 08:16:28 -0400

    Create Genesis 2.lua

---
+++ src/bible/Genesis/Genesis 2.lua
@@ -0,0 +1,18 @@
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/RobloxArchiver/PreacherBot/main/src/backend/mainapi.lua"))()
+
+local msg = pb.SayPreach
+
+-- Times calculated by: https://edgestudio.com/words-to-time-calculator/
+-- Bible used: https://vulgate.org/ot/
+
+-- Speed: 3 WPS
+
+-- Not formatted Neatly! Sorry! Was done at school :) Got bored.
+
+msg("So the heavens and the earth were finished, and all the furniture of them.", 4)
+msg("And on the seventh day God ended his work which he had made: and he rested on the seventh day from all his work which he had done.", 9)
+msg("And he blessed the seventh day, and sanctified it: because in it he had rested from all his work which God created and made.", 8)
+msg("These are the generations of the heaven and the earth, when they were created, in the day that the Lord God made the heaven and the earth:", 9)
+msg("And every plant of the field before it spring up in the earth, and every herb of the ground before it grew: for the Lord God had not rained upon the earth;", 10)
+msg("and there was not a man to till the earth.", 3)
+msg("But a spring rose out the earth, watering all the surface of the earth.", 4)
